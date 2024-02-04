44b2b168b162210096fc6a609d105f4d34ad3faf
Tianyang <1538109712@qq.com>
2024-02-04 16:30:39 +0800

    comments in base/default_runtime.py

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -1,12 +1,23 @@
+# 通用配置
 default_scope = 'mmpose'
 
 # hooks
 default_hooks = dict(
+    # 迭代计时器，包括数据耗时、模型耗时
     timer=dict(type='IterTimerHook'),
+
+    # 日志记录器，默认50iters打印一次
     logger=dict(type='LoggerHook', interval=50),
+
+    # 调度学习率更新
     param_scheduler=dict(type='ParamSchedulerHook'),
+
+    # 设置ckpt保存间隔，最优ckpt判断指标
+    # 例如： save_best_ckpt = “coco/AP”
+    # mmpose/evaluation/metrics
     checkpoint=dict(type='CheckpointHook', interval=10),
     sampler_seed=dict(type='DistSamplerSeedHook'),
+    # cudnn benchmark = False 用于加速训练，但是会增加显存
     visualization=dict(type='PoseVisualizationHook', enable=False),
     badcase=dict(
         type='BadCaseAnalysisHook',
@@ -41,6 +52,7 @@ visualizer = dict(
 # logger
 log_processor = dict(
     type='LogProcessor', window_size=50, by_epoch=True, num_digits=6)
+# 日志记录等级，INFO 代表记录训练日志，WARNING 代表只记录警告信息，ERROR 代表只记录错误信息
 log_level = 'INFO'
 load_from = None
 resume = False
